56e0d575c38a3a0c92b11f8e91150e2426c6a97e
probablyarth <arththapa7@gmail.com>
2023-05-26 22:20:59 +0530

    Add Button

---
+++ src/lib/components/Button.tsx
@@ -0,0 +1,37 @@
+import type { FC, HTMLProps } from 'react';
+
+type variant = 'primary' | 'secondary';
+type size = 'small' | 'medium' | 'full';
+const types = {
+  primary: 'bg-primary text-white',
+  secondary: 'bg-secondary text-white',
+};
+
+const sizes = {
+  small: 'max-w-[200px] w-full py-[14px]',
+  medium: 'max-w-[300px] w-full py-[19px]',
+  full: 'w-full',
+};
+
+type ButtonProps = {
+  variant?: variant;
+  buttonSize?: size;
+};
+
+const Button: FC<HTMLProps<HTMLButtonElement> & ButtonProps> = ({
+  onClick,
+  children,
+  variant = 'primary',
+  buttonSize = 'small',
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`text-level-6 font-bold px-[20px] min-w-[100px] rounded-md py-[19px] w-full shadow-sm ${types[variant]} ${sizes[buttonSize]}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,27 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        'level-1': 96,
+        'level-2': 64,
+        'level-3': 48,
+        'level-4': 36,
+        'level-5': 24,
+        'level-6': 18,
+      },
+      colors: {
+        primary: '#3E3636',
+        secondary: '#D72323',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
       },
     },
   },
   plugins: [],
-}
+};
